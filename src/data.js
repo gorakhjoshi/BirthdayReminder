3327213090594431417270990e0b60d25ecd84f3
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-02 20:29:48 +0545

    Initial Setup

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Bibek Baidya',
+    age: 25,
+    image:
+      'https://avatars.githubusercontent.com/u/85629481?v=4',
+  },
+  {
+    id: 2,
+    name: 'Santosh Nepal',
+    age: 24,
+    image:
+      'https://avatars.githubusercontent.com/u/46523707?v=4',
+  },
+  {
+    id: 3,
+    name: 'Bidur Parajuli',
+    age: 36,
+    image:
+      'https://avatars.githubusercontent.com/u/83571599?v=4',
+  },
+  {
+    id: 4,
+    name: 'Sanjit Katwal',
+    age: 26,
+    image:
+      'https://avatars.githubusercontent.com/u/6796470?v=4',
+  },
+  {
+    id: 5,
+    name: 'Binaya Maharjan',
+    age: 25,
+    image:
+      'https://avatars.githubusercontent.com/u/45600506?v=4',
+  },
+];
