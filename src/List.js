1d1c6e7930dc21dd39fd530d81a13432f4f4c6b4
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-09-13 20:11:27 +0545

    removed console message

--- src/List.js
+++ src/List.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const List = ({ people }) => {
-  console.log(people)
-
   return (
     <>
       {people.map((person) => {
-        const { id, name, age, image } = person
+        const { id, name, age, image } = person;
         return (
           <article key={id} className='person'>
             <img src={image} alt={name} />
@@ -15,10 +13,10 @@ const List = ({ people }) => {
               <p>{age}</p>
             </div>
           </article>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
 
-export default List
+export default List;
